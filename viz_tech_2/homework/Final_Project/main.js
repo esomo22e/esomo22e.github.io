3945af5aca5a1c3ac73ee2fdaba29c4d6747d582
Eunice Esomonu <eesomonu@gmail.com>
2020-04-05 17:34:25 -0400

    fixed network

--- viz_tech_2/homework/Final_Project/main.js
+++ viz_tech_2/homework/Final_Project/main.js
@@ -1,8 +1,7 @@
 var width = d3.select('#graph').node().offsetWidth;
-// console.log(d3.select('#graph').node().offsetWidth)
-var width = 500;
-var height = window.innerHeight * 0.7;
+var height =  d3.select('#graph').node().offsetHeight;
 
+// height = 500;
 var margin = {
   top: 20,
   right: 20,
@@ -15,219 +14,173 @@ var margin = {
 
 
 var svg = d3.select("#graph")
-// .append('svg')
-.attr("width", width)
-.attr("height", height);
+  .append('svg')
+  .attr("width", width)
+  .attr("height", height);
 
-var radius = 5;
-    var forceStrength = 0.05;
+var radius = 8;
+var forceStrength = 0.3;
 
 
 d3.queue()
-// .defer(d3.csv, "./data/test.csv")
-.defer(d3.csv, "./data/clothes_perc.csv")
-.defer(d3.csv, "./data/words_perc.csv")
-.defer(d3.csv, "./data/schools.csv")
-.await(function(error, data_clothes, data_words, data_schools) {
-  // console.log(data_test);
-  console.log(data_clothes);
-  console.log(data_words);
-  console.log(data_schools);
-
-
-
-  function canvas_clear(){
-
-  svg
-      .selectAll("*")
-      .remove();
-  }
-
-  function sec_1(){
-    console.log("section 1")
-    canvas_clear();
-
-    data_schools.forEach(function(d){
-      d.r = radius;
-      d.x = width/2;
-      d.y = height/2;
-    })
-
+  .defer(d3.csv, "./data/test.csv")
+  .defer(d3.csv, "./data/clothes_perc.csv")
+  .defer(d3.csv, "./data/words_perc.csv")
+  .defer(d3.csv, "./data/schools.csv")
+  .await(function(error, data_test, data_clothes, data_words, data_schools) {
+    // console.log(data_test);
+    console.log(data_clothes);
+    console.log(data_words);
     console.log(data_schools);
 
-    var simulation = d3.forceSimulation()
-           .force("collide",d3.forceCollide( function(d){
-               return d.r + 8 }).iterations(16)
-           )
-           .force("charge", d3.forceManyBody())
-           .force("y", d3.forceY().y(height / 2))
-           .force("x", d3.forceX().x(width / 2));
+
+
+    function canvas_clear() {
+
+      svg
+        .selectAll("*")
+        .remove();
+    }
+
+    function sec_1() {
+      console.log("section 1")
+      canvas_clear();
+
+      data_schools.forEach(function(d) {
+        d.r = radius;
+        d.x = width / 2;
+        d.y = height / 2;
+      })
+
+      console.log(data_schools);
+
+      var simulation = d3.forceSimulation()
+        .force("collide", d3.forceCollide(function(d) {
+          return d.r + 2;
+        }).iterations(16))
+        .force("charge", d3.forceManyBody())
+        .force("y", d3.forceY().y(height / 4))
+        .force("x", d3.forceX().x(width / 4));
 
       var circles = svg.selectAll("circle")
-      	.data(data_schools);
+        .data(data_schools);
 
 
       var circlesEnter = circles.enter().append("circle")
-      	.attr("r", function(d, i){
-           return d.r;
-         })
-       .attr("cx", function(d, i){ return 175 + 25 * i + 2 * i ** 2; })
-				.attr("cy", function(d, i){ return 250; })
-      	.style("fill", function(d, i){ return "#000"; })
-      	.style("stroke", function(d, i){ return "#000"; });
+        .attr("r", function(d, i) {
+          return d.r;
+        })
+        .attr("cx", function(d, i) {
+          return 175 + 25 * i + 2 * i ** 2;
+        })
+        .attr("cy", function(d, i) {
+          return 250;
+        })
+        .style("fill", function(d, i) {
+          return "#000";
+        })
+        .style("stroke", function(d, i) {
+          return "#000";
+        });
 
-        circles = circles.merge(circlesEnter);
+      circles = circles.merge(circlesEnter);
 
 
-  function ticked() {
-    //console.log("tick")
-    //console.log(data.map(function(d){ return d.x; }));
-    circles
-        .attr("cx", function(d){
-           return d.x;
+      function ticked() {
+        //console.log("tick")
+        //console.log(data.map(function(d){ return d.x; }));
+        circles
+          .attr("cx", function(d) {
+            return d.x = Math.max(d.r, Math.min(width - d.r, d.x));
 
-         })
-        .attr("cy", function(d){
-           return d.y;
-         });
-  }
+          })
+          .attr("cy", function(d) {
+            return d.y = Math.max(d.r, Math.min(height - d.r, d.y));
+          });
+      }
 
-  simulation
-            .nodes(data_schools)
-            .on("tick", ticked);
+      simulation
+        .nodes(data_schools)
+        .on("tick", ticked);
 
-            function groupBubbles() {
-            // hideTitles();
+      function groupBubbles() {
+        // hideTitles();
 
-            // @v4 Reset the 'x' force to draw the bubbles to the center.
-            simulation.force('x', d3.forceX().strength(forceStrength).x(width / 2));
+        // @v4 Reset the 'x' force to draw the bubbles to the center.
+        simulation
+          .force('x', d3.forceX().strength(forceStrength).x(width / 2))
+          .force('y', d3.forceY()
+            .strength(forceStrength)
+            .y(height / 2));
+        // @v4 We can reset the alpha value and restart the simulation
+        simulation.alpha(1).restart();
+      }
 
-            // @v4 We can reset the alpha value and restart the simulation
-            simulation.alpha(1).restart();
-          }
-
-          // groupBubbles();
-    // var nodes = svg.selectAll("circle")
-    //                  .data(data_schools);
-    //
-    //    var node_circle = nodes.enter().append("circle")
-    //          	.attr("r", function(d, i){ return d.r; })
-    //                          .attr("cx", function(d, i){
-    //                              return 175 + 25* i + 2
-    //                          })
-    //                          .attr("cx", function(d, i){ return 175 + 25 * i + 2 * i ** 2; })
-    //                            .attr("cy", function(d, i){ return 250; })
-    //                            // .attr("xlink:href",  function(d) { return icon[d.gender];})
-    //                            .attr("height", 130)
-    //                            .attr("width", 130);
-    //
-    //        nodes = nodes.merge(node_circle);
-    //
-    //        function ticked(){
-    //                nodes
-    //                .attr("x", function(d){
-    //                  // console.log(d);
-    //                   return d.x = Math.max(d.r, Math.min(width - d.r, d.x)); })
-    //               .attr("y", function(d){ return d.y = Math.max(d.r, Math.min(height - d.r, d.y)); })
-    //        }
-    //
-    //        var simulation = d3
-    //               .forceSimulation()
-    //               //keeping the commented lines below in case I need them later
-    //               .force("collide",d3.forceCollide( function(d){
-    //                  return d.r + 0 }).iterations(16))
-    //               //d.r is defined as radius above
-    //               .force("charge", d3.forceManyBody()
-    //                 .strength(-10 ))
-    //
-    //                 simulation
-    //                       .nodes(data_schools)
-    //                       .on("tick", ticked);
-    //
-    //             var forceStrength = //1;
-    //                                  0.05;
-    //
-    //
-    //                 function groupBubbles() {
-    //
-    //                   simulation.force('x', d3.forceX()
-    //                                         .strength(forceStrength)
-    //                                         .x(width / 2))
-    //                           .force('y', d3.forceY()
-    //                                         .strength(forceStrength)
-    //                                         .y(width / 2));
-    //
-    //               simulation
-    //                   .alpha(1)
-    //                   .restart();
-    //                   // .restart();
-    //
-    //
-    //
-    //                 }
-    //
-    //           groupBubbles();
+      groupBubbles();
 
 
 
-  }
 
-  function sec_2(){  //who's won more
-    canvas_clear();
+
+    }
+
+    function sec_2() { //who's won more
+      canvas_clear();
 
       console.log("section 2")
 
-  }
+    }
 
-  function sec_3(){
-    canvas_clear();
+    function sec_3() {
+      canvas_clear();
 
-          console.log("section 3")
+      console.log("section 3")
 
-  }
+    }
 
-  function sec_4(){
-    canvas_clear();
+    function sec_4() {
+      canvas_clear();
 
       console.log("section 4")
 
 
-  }
+    }
 
-  function sec_5(){
+    function sec_5() {
 
-    canvas_clear();
+      canvas_clear();
 
       console.log("section 5")
 
 
-  }
+    }
 
 
 
-  var gs = d3.graphScroll()
+    var gs = d3.graphScroll()
       .container(d3.select('#container'))
       .graph(d3.selectAll('#graph'))
       .sections(d3.selectAll('#sections > div'))
       // .offset(innerWidth < 900 ? innerHeight - 30 : 200)
       .eventId('uniqueId1')
-      .on('active', function(i){
+      .on('active', function(i) {
 
 
 
-              [
-                  sec_1,
-                sec_2,
-                sec_3,
-                sec_4,
-                sec_5 ][i]();
+        [
+          sec_1,
+          sec_2,
+          sec_3,
+          sec_4,
+          sec_5
+        ][i]();
+
+      });
+
+
+
 
   });
-
-
-
-
-});
 
 
 // var oldWidth = 0
