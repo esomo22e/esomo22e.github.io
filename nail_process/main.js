d6d895548d593bf3bb5e454d777f3f63f54a4845
eesomonu1994 <e.esomonu@northeastern.edu>
2020-12-01 21:54:16 -0500

    add initial image for tile 1

--- nail_process/main.js
+++ nail_process/main.js
@@ -3,45 +3,124 @@
 var s = function( p ) { // p could be any variable name
   var x = 100;
   var y = 100;
+  var header;
+  var text = 'NAIL PROCESS';
+  var text2 = 'NAIL PROCESS';
+  var textSize;
+  var font;
+  var img;
+  var img2;
 
-  var text = 'Nail Process';
+  var tileW, tileHt, centerItemX, centerItemY;
+
+
+  p.preload = function(){
+    img =  p.loadImage('assets/hand_1.png');
+    img2 =  p.loadImage('assets/hand_2.png');
+
+
+  }
+
   p.setup = function() {
+
     p.createCanvas(p.windowWidth/3.05, p.windowHeight/3.05);
+    font = 'Helvetica';
+
+    tileW = (p.windowWidth/3.05);
+    tileH = (p.windowHeight/3.05);
+    centerItemX = ((p.windowWidth/3.05) - (p.windowWidth/6.10));
+    centerItemY = p.windowHeight/6.05 + 10;
+    // console.log(p.windowWidth/3.05);
+    // console.log(p.windowHeight);
+    //
+    // console.log("WIDTH/2:" + ((p.windowWidth/3.05) - (p.windowWidth/6.10)));
+
+
+
   };
 
   p.draw = function() {
 
 
-    // p.background(0);
-    // p.fill(255);
-    // p.rect(x,y,50,50);
-    //
+      p.background(255, 151, 148);
+        p.imageMode(p.CENTER);
+      p.image(img, centerItemX, centerItemY,300,300);
 
+    if ((p.mouseX > 0) && (p.mouseX < tileW) && (p.mouseY > 0) && (p.mouseY < tileH)){
 
+      p.background(120,20,231);
+      p.imageMode(p.CENTER);
 
-    if ((p.mouseX > 0) && (p.mouseX < (p.windowWidth/3.05)) && (p.mouseY > 0) && (p.mouseY < (p.windowHeight/3.05))){
-      // p.background(50,120,120);
-      // p.fill(124,45,230);
-      // p.rect(x,y,50,50);
+      p.image(img2, centerItemX,centerItemY,300,300);
 
-      p.textSize(mouseX);
+      // text = "";
+      textSize = 40;
+      textSize = textSize + 20;
+      p.textSize(textSize);
+      // p.textFont(font);
+      p.textFont("sans-serif");
+      // p.textFont("bold");
         p.fill(255);
-        p.text(text, 0, p.windowHeight/6.05);
-    }
+        p.textAlign(p.CENTER);
+        p.text(text, centerItemX, centerItemY);
+
+
+
+
+      }
     else{
 
       //
-      p.background(0);
-      // p.fill(255);
-      // p.rect(x,y,50,50);
+      p.background(255, 151, 148);
+      p.imageMode(p.CENTER);
+      p.image(img, centerItemX, centerItemY,300,300);
 
-      p.textSize(52);
+
+      textSize = 40;
+      p.textSize(textSize);
+      // p.textFont(font);
+      p.textFont("sans-serif");
         p.fill(255);
-      p.text(text, 0, p.windowHeight/6.05);
+        p.textAlign(p.CENTER);
+
+        p.text(text, centerItemX, centerItemY);
+
+
+
     }
+
+
   };
+
+  // p.oldText = function(){
+  //   p.textSize(52);
+  //     p.fill(255);
+  //     p.text(text, 0, p.windowHeight/6.05);
+  //
+  //
+  //
+  //
+  // }
+  //
+  // p.newText = function(){
+  //   p.textSize(60);
+  //     p.fill(200, 80, 255);
+  //     p.text(text2, 0, p.windowHeight/6.05);
+  //
+  //
+  //
+  // }
 };
+
+
 var myp5 = new p5(s, 'sketch1');
+
+
+
+
+
+
+
 
 // Sketch Two
 var t = function( p ) {
@@ -53,11 +132,11 @@ var t = function( p ) {
   };
 
   p.draw = function() {
-    p.fill(1);
-     p.background(100);
+    // p.fill(1);
+    p.background(255, 151, 148);
      // console.log(p.windowWidth/3.05)
      // console.log(p.windowWidth * (2/3.05))
-     if ((p.mouseX >0) && (p.mouseX < (p.windowWidth/3.05)) && (p.mouseY > 0) && (p.mouseY < (p.windowHeight/3.05))){
+     if ((p.mouseX >0) && (p.mouseX < tileW) && (p.mouseY > 0) && (p.mouseY < tileH)){
 
        p.background(50,50,100);
 
@@ -68,13 +147,14 @@ var t = function( p ) {
        p.ellipse(x,y,50,50);
 }
 else{
-  p.background(100);
+  // p.background(100);
+  p.background(255, 151, 148);
 
 }
 
 
     //   if ((p.mouseX > 0) && (p.mouseX < (p.windowWidth * (2/3.05))) &&
-    //     (p.mouseY > 0) && (p.mouseY < (p.windowWidth/3.05))){
+    //     (p.mouseY > 0) && (p.mouseY < tileW)){
     // p.background(150,200,189);
     //
     // x += speed;
@@ -103,9 +183,9 @@ var t = function( p ) {
   };
 
   p.draw = function() {
-    p.background(20,10,100);
+    p.background(255, 151, 148);
     p.fill(1);
-    if ((p.mouseX >0) && (p.mouseX < (p.windowWidth/3.05)) && (p.mouseY > 0) && (p.mouseY < (p.windowHeight/3.05))){
+    if ((p.mouseX >0) && (p.mouseX < tileW) && (p.mouseY > 0) && (p.mouseY < tileH)){
 
       p.background(250,250,100);
 
@@ -116,7 +196,7 @@ var t = function( p ) {
       p.ellipse(x,y,50,50);
 }
 else{
-  p.background(20,10,100);
+  p.background(255, 151, 148);
 
 }
 
