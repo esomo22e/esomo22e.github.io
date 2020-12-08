2ce62c3712197e19512a3f8770c96601b0e3121b
Eunice Esomonu <eesomonu@gmail.com>
2020-12-08 01:39:31 -0500

    test gifs

--- nail_process/js/main.js
+++ nail_process/js/main.js
@@ -1,35 +1,37 @@
 // save this file as sketch.js
 // Sketch One
-  var tileW, tileHt, centerItemX, centerItemY;
-  var textSize = 40;
+var tileW, tileHt, centerItemX, centerItemY;
+var textSize = 50;
+// ;
 
-var s = function( p ) { // p could be any variable name
+var s = function(p) { // p could be any variable name
   var x = 100;
   var y = 100;
   var header;
   var num = '1';
   var font;
   var img, img2;
+  var gif_createImg;
 
 
+  p.preload = function() {
+    img = p.loadImage('assets/tile-1-hands.png');
+    img2 = p.loadImage('assets/tile-1-hands.gif');
 
-
-  p.preload = function(){
-    img =  p.loadImage('assets/hand_1.png');
-    img2 =  p.loadImage('assets/hand_2.png');
+    // gif_createImg = createImg("tile-1-hands.gif");
 
 
   }
 
   p.setup = function() {
 
-    p.createCanvas(window.innerWidth/3.05, window.innerHeight/3.05);
+    p.createCanvas(window.innerWidth / 3.05, window.innerHeight / 3.05);
     font = 'Helvetica';
 
-    tileW = (window.innerWidth/3.05);
-    tileH = (window.innerHeight/3.05);
-    centerItemX = ((window.innerWidth/3.05) - (window.innerWidth/6.10));
-    centerItemY = window.innerHeight/6.05 + 10;
+    tileW = (window.innerWidth / 3.05);
+    tileH = (window.innerHeight / 3.05);
+    centerItemX = ((window.innerWidth / 3.05) - (window.innerWidth / 6.10));
+    centerItemY = window.innerHeight / 6.05;
     // console.log(window.innerWidth/3.05);
     // console.log(window.innerHeight);
     //
@@ -42,24 +44,35 @@ var s = function( p ) { // p could be any variable name
   p.draw = function() {
 
 
-      p.background(255, 151, 148);
-        p.imageMode(p.CENTER);
-      p.image(img, centerItemX, centerItemY,300,300);
-      // textSize = 40;
-
-      p.textSize(textSize);
-
-      p.text(num, centerItemX, centerItemY);
+    p.background(255, 151, 148);
 
 
-    if ((p.mouseX > 0) && (p.mouseX < tileW) && (p.mouseY > 0) && (p.mouseY < tileH)){
+    p.imageMode(p.CENTER);
+    p.image(img, centerItemX, centerItemY + 15, window.innerWidth / 3.5, window.innerHeight / 3.5);
+    // textSize = 40;
+
+    p.rectMode(p.CENTER);
+    p.fill(100);
+    // p.rect(centerItemX, centerItemY-10, window.innerWidth/3.05, window.innerHeight/3.05);
+
+    p.rect(centerItemX, centerItemY, 100, 100);
+
+    p.fill(255);
+    p.textAlign(p.CENTER);
+    p.textSize(textSize);
+
+    p.text(num, centerItemX, centerItemY + 20);
+
+
+
+    if ((p.mouseX > 0) && (p.mouseX < tileW) && (p.mouseY > 0) && (p.mouseY < tileH)) {
 
       // p.background(120,20,231);
       p.background(255, 151, 148);
 
       p.imageMode(p.CENTER);
 
-      p.image(img2, centerItemX,centerItemY,300,300);
+      p.image(img2, centerItemX, centerItemY + 15, window.innerWidth / 3.5, window.innerHeight / 3.5);
 
       // text = "";
       // textSize = 40;
@@ -70,20 +83,19 @@ var s = function( p ) { // p could be any variable name
       // // p.textFont("bold");
       //   p.fill(255);
       //   p.textAlign(p.CENTER);
-        // p.text(text, centerItemX, centerItemY);
-        num = "";
-        p.textSize(textSize);
-        p.text(num, centerItemX, centerItemY);
+      // p.text(text, centerItemX, centerItemY);
+      num = "";
+      p.textSize(textSize);
+      p.text(num, centerItemX, centerItemY);
 
 
 
-      }
-    else{
+    } else {
 
       //
       // p.background(255, 151, 148);
       p.imageMode(p.CENTER);
-      p.image(img, centerItemX, centerItemY,300,300);
+      p.image(img, centerItemX, centerItemY + 15, window.innerWidth / 3.5, window.innerHeight / 3.5);
 
 
       // textSize = 40;
@@ -95,7 +107,7 @@ var s = function( p ) { // p could be any variable name
       //
       //   p.text(text, centerItemX, centerItemY);
       num = "1";
-      p.text(num, centerItemX, centerItemY);
+      p.text(num, centerItemX, centerItemY + 20);
 
 
 
@@ -118,53 +130,76 @@ var myp5 = new p5(s, 'sketch1');
 
 
 // Sketch Two
-var t = function( p ) {
+var t = function(p) {
   var x = 100.0;
   var y = 100;
   var num2 = '2';
+  var img3, img4;
 
   var speed = 2.5;
+  p.preload = function() {
+    img3 = p.loadImage('assets/tile-2-water.png');
+    img4 = p.loadImage('assets/hand_2.png');
+
+
+  }
+
   p.setup = function() {
-    p.createCanvas(window.innerWidth/3.05, window.innerHeight/3.05);
+    p.createCanvas(window.innerWidth / 3.05, window.innerHeight / 3.05);
+
+    tileW = (window.innerWidth / 3.05);
+    tileH = (window.innerHeight / 3.05);
+    centerItemX = ((window.innerWidth / 3.05) - (window.innerWidth / 6.10));
+    centerItemY = window.innerHeight / 6.05;
   };
 
   p.draw = function() {
     // p.fill(1);
     p.background(255, 151, 148);
-     // console.log(window.innerWidth/3.05)
-     // console.log(window.innerWidth * (2/3.05))
-     p.textSize(textSize);
+    // console.log(window.innerWidth/3.05)
+    // console.log(window.innerWidth * (2/3.05))
+    p.imageMode(p.CENTER);
+    p.image(img3, centerItemX, centerItemY - 5, window.innerWidth / 4.5, window.innerHeight / 3.25);
+    // textSize = 40;
+
+    p.rectMode(p.CENTER);
+    p.fill(100);
+    // p.rect(centerItemX, centerItemY-10, window.innerWidth/3.05, window.innerHeight/3.05);
+
+    p.rect(centerItemX, centerItemY, 100, 100);
+
+    p.fill(255);
+    p.textAlign(p.CENTER);
+    p.textSize(textSize);
+
+    p.text(num2, centerItemX, centerItemY + 20);
+
+    if ((p.mouseX > 0) && (p.mouseX < tileW) && (p.mouseY > 0) && (p.mouseY < tileH)) {
+
+      // p.background(50,50,100);
+      p.background(255, 151, 148);
+
+      p.imageMode(p.CENTER);
+
+      p.image(img4, centerItemX, centerItemY, 300, 300);
 
 
-     p.text(num2, centerItemX, centerItemY);
+      num = "";
+      p.textSize(textSize);
+      p.text(num2, centerItemX, centerItemY);
 
-     if ((p.mouseX >0) && (p.mouseX < tileW) && (p.mouseY > 0) && (p.mouseY < tileH)){
+    } else {
+      // p.background(100);
+      // p.background(255, 151, 148);
+      p.imageMode(p.CENTER);
+      p.image(img3, centerItemX, centerItemY - 5, window.innerWidth / 4.5, window.innerHeight / 3.25);
 
-       // p.background(50,50,100);
-       p.background(255, 151, 148);
+      num2 = "2";
+      p.textSize(textSize);
 
-       // x += speed;
-       // if(x > p.width){
-       //   x = 0;
-       // }
-       // p.ellipse(x,y,50,50);
+      p.text(num2, centerItemX, centerItemY + 20);
 
-       num2 = "";
-       p.textSize(textSize);
-
-       p.text(num2, centerItemX, centerItemY);
-
-}
-else{
-  // p.background(100);
-  p.background(255, 151, 148);
-
-  num2 = "2";
-  p.textSize(textSize);
-
-  p.text(num2, centerItemX, centerItemY);
-
-}
+    }
 
 
 
@@ -172,49 +207,81 @@ else{
 };
 var myp5 = new p5(t, 'sketch2');
 
+
+
 // Sketch Three
-var t = function( p ) {
+var t = function(p) {
   var x = 100.0;
   var y = 100;
-  var speed = 2.5;
   var num3 = '3';
+  var img5, img6;
+
+  var speed = 2.5;
+  p.preload = function() {
+    img5 = p.loadImage('assets/tile-3-file.png');
+    img6 = p.loadImage('assets/hand_2.png');
+
+
+  }
 
   p.setup = function() {
-    p.createCanvas(window.innerWidth/3.05, window.innerHeight/3.05);
+    p.createCanvas(window.innerWidth / 3.05, window.innerHeight / 3.05);
+
+    tileW = (window.innerWidth / 3.05);
+    tileH = (window.innerHeight / 3.05);
+    centerItemX = ((window.innerWidth / 3.05) - (window.innerWidth / 6.10));
+    centerItemY = window.innerHeight / 6.05;
   };
 
   p.draw = function() {
+    // p.fill(1);
     p.background(255, 151, 148);
-    p.fill(1);
+    // console.log(window.innerWidth/3.05)
+    // console.log(window.innerWidth * (2/3.05))
+    p.imageMode(p.CENTER);
+    p.image(img5, centerItemX+20, centerItemY , window.innerWidth / 3, window.innerHeight / 3.25);
+    // textSize = 40;
 
+    p.rectMode(p.CENTER);
+    p.fill(100);
+    // p.rect(centerItemX, centerItemY-10, window.innerWidth/3.05, window.innerHeight/3.05);
+
+    p.rect(centerItemX, centerItemY, 100, 100);
+
+    p.fill(255);
+    p.textAlign(p.CENTER);
     p.textSize(textSize);
 
-    p.text(num3, centerItemX, centerItemY);
-    if ((p.mouseX >0) && (p.mouseX < tileW) && (p.mouseY > 0) && (p.mouseY < tileH)){
+    p.text(num3, centerItemX, centerItemY + 20);
 
-      // p.background(250,250,100);
+    if ((p.mouseX > 0) && (p.mouseX < tileW) && (p.mouseY > 0) && (p.mouseY < tileH)) {
+
+      // p.background(50,50,100);
       p.background(255, 151, 148);
 
+      p.imageMode(p.CENTER);
 
-      // x += speed;
-      // if(x > p.width){
-      //   x = 0;
-      // }
-      // p.ellipse(x,y,50,50);
+      p.image(img6, centerItemX, centerItemY, 300, 300);
+
+
       num3 = "";
       p.textSize(textSize);
-
       p.text(num3, centerItemX, centerItemY);
-}
-else{
-  p.background(255, 151, 148);
 
-  num3 = "3";
-  p.textSize(textSize);
+    } else {
+      // p.background(100);
+      // p.background(255, 151, 148);
+      p.imageMode(p.CENTER);
+      p.image(img5, centerItemX+20, centerItemY , window.innerWidth / 3, window.innerHeight / 3.25);
 
-  p.text(num3, centerItemX, centerItemY);
+      num3 = "3";
+      p.textSize(textSize);
 
-}
+      p.text(num3, centerItemX, centerItemY + 20);
+
+    }
+
+
 
   };
 };
--- nail_process/js/main3.js
+++ nail_process/js/main3.js
@@ -1,86 +1,257 @@
 // save this file as sketch.js
 // Sketch Seven
-var s = function( p ) { // p could be any variable name
+var tileW, tileHt, centerItemX, centerItemY;
+var textSize = 50;
+// ;
+
+var s = function(p) { // p could be any variable name
   var x = 100;
   var y = 100;
+  var header;
+  var num = '7';
+  var font;
+  var img, img2;
+
+
+
+  p.preload = function() {
+    img = p.loadImage('assets/tile-7-glue.png');
+    img2 = p.loadImage('assets/hand_2.png');
+
+
+  }
+
   p.setup = function() {
-    p.createCanvas(window.innerWidth/3.05, window.innerHeight/3.05);
+
+    p.createCanvas(window.innerWidth / 3.05, window.innerHeight / 3.05);
+    font = 'Helvetica';
+
+    tileW = (window.innerWidth / 3.05);
+    tileH = (window.innerHeight / 3.05);
+    centerItemX = ((window.innerWidth / 3.05) - (window.innerWidth / 6.10));
+    centerItemY = window.innerHeight / 6.05;
+    // console.log(window.innerWidth/3.05);
+    // console.log(window.innerHeight);
+    //
+    // console.log("WIDTH/2:" + ((window.innerWidth/3.05) - (window.innerWidth/6.10)));
+
+
+
   };
 
   p.draw = function() {
-    p.background(0);
-    p.fill(255);
-    p.rect(x,y,50,50);
 
-    if ((p.mouseX > 0) && (p.mouseX < (window.innerWidth/3.05)) && (p.mouseY > 0) && (p.mouseY < (window.innerHeight/3.05))){
-      p.background(150,120,220);
-      p.fill(224,45,130);
-      p.rect(x,y,50,50);
+
+    p.background(255, 151, 148);
+
+
+    p.imageMode(p.CENTER);
+    p.image(img, centerItemX, centerItemY + 15, window.innerWidth / 3.5, window.innerHeight / 3.5);
+    // textSize = 40;
+
+    p.rectMode(p.CENTER);
+    p.fill(100);
+    // p.rect(centerItemX, centerItemY-10, window.innerWidth/3.05, window.innerHeight/3.05);
+
+    p.rect(centerItemX, centerItemY, 100, 100);
+
+    p.fill(255);
+    p.textAlign(p.CENTER);
+    p.textSize(textSize);
+
+    p.text(num, centerItemX, centerItemY + 20);
+
+
+
+    if ((p.mouseX > 0) && (p.mouseX < tileW) && (p.mouseY > 0) && (p.mouseY < tileH)) {
+
+      // p.background(120,20,231);
+      p.background(255, 151, 148);
+
+      p.imageMode(p.CENTER);
+
+      p.image(img2, centerItemX, centerItemY, 300, 300);
+
+      num = "";
+      p.textSize(textSize);
+      p.text(num, centerItemX, centerItemY);
+
+
+
+    } else {
+
+      //
+      // p.background(255, 151, 148);
+      p.imageMode(p.CENTER);
+      p.image(img, centerItemX, centerItemY + 15, window.innerWidth / 3.5, window.innerHeight / 3.5);
+
+
+
+      num = "7";
+      p.text(num, centerItemX, centerItemY + 20);
+
+
+
     }
-    else{
-      p.background(0);
-      p.fill(255);
-      p.rect(x,y,50,50);
-    }
+
+
   };
+
+
 };
 var myp5 = new p5(s, 'sketch7');
 
 // Sketch Eight
-var t = function( p ) {
+var t = function(p) {
   var x = 100.0;
   var y = 100;
+  var num2 = '8';
+  var img3, img4;
+
   var speed = 2.5;
+  p.preload = function() {
+    img3 = p.loadImage('assets/tile-8-press.png');
+    img4 = p.loadImage('assets/hand_2.png');
+
+
+  }
+
   p.setup = function() {
-    p.createCanvas(window.innerWidth/3.05, window.innerHeight/3.05);
+    p.createCanvas(window.innerWidth / 3.05, window.innerHeight / 3.05);
+
+    tileW = (window.innerWidth / 3.05);
+    tileH = (window.innerHeight / 3.05);
+    centerItemX = ((window.innerWidth / 3.05) - (window.innerWidth / 6.10));
+    centerItemY = window.innerHeight / 6.05;
   };
 
   p.draw = function() {
-    p.background(100);
-    p.fill(1);
-    if ((p.mouseX > 0) && (p.mouseX < (window.innerWidth/3.05)) && (p.mouseY > 0) && (p.mouseY < (window.innerHeight/3.05))){
-      p.background(200, 50, 137);
+    // p.fill(1);
+    p.background(255, 151, 148);
+    // console.log(window.innerWidth/3.05)
+    // console.log(window.innerWidth * (2/3.05))
+    p.imageMode(p.CENTER);
+    p.image(img3, centerItemX, centerItemY - 5, window.innerWidth / 4.5, window.innerHeight / 3.25);
+    // textSize = 40;
 
-      x += speed;
-      if(x > p.width){
-        x = 0;
-      }
-      p.ellipse(x,y,50,50);
-    }
-    else{
-      p.background(100);
+    p.rectMode(p.CENTER);
+    p.fill(100);
+    // p.rect(centerItemX, centerItemY-10, window.innerWidth/3.05, window.innerHeight/3.05);
+
+    p.rect(centerItemX, centerItemY, 100, 100);
+
+    p.fill(255);
+    p.textAlign(p.CENTER);
+    p.textSize(textSize);
+
+    p.text(num2, centerItemX, centerItemY + 20);
+
+    if ((p.mouseX > 0) && (p.mouseX < tileW) && (p.mouseY > 0) && (p.mouseY < tileH)) {
+
+      // p.background(50,50,100);
+      p.background(255, 151, 148);
+
+      p.imageMode(p.CENTER);
+
+      p.image(img4, centerItemX, centerItemY, 300, 300);
+
+
+      num = "";
+      p.textSize(textSize);
+      p.text(num2, centerItemX, centerItemY);
+
+    } else {
+      // p.background(100);
+      // p.background(255, 151, 148);
+      p.imageMode(p.CENTER);
+      p.image(img3, centerItemX, centerItemY - 5, window.innerWidth / 4.5, window.innerHeight / 3.25);
+
+      num2 = "8";
+      p.textSize(textSize);
+
+      p.text(num2, centerItemX, centerItemY + 20);
 
     }
+
+
 
   };
 };
 var myp5 = new p5(t, 'sketch8');
 
 // Sketch Nine
-var t = function( p ) {
+var t = function(p) {
   var x = 100.0;
   var y = 100;
+  var num3 = '9';
+  var img5, img6;
+
   var speed = 2.5;
+  p.preload = function() {
+    img5 = p.loadImage('assets/tile-9-fake-complete.png');
+    img6 = p.loadImage('assets/hand_2.png');
+
+
+  }
+
   p.setup = function() {
-    p.createCanvas(window.innerWidth/3.05, window.innerHeight/3.05);
+    p.createCanvas(window.innerWidth / 3.05, window.innerHeight / 3.05);
+
+    tileW = (window.innerWidth / 3.05);
+    tileH = (window.innerHeight / 3.05);
+    centerItemX = ((window.innerWidth / 3.05) - (window.innerWidth / 6.10));
+    centerItemY = window.innerHeight / 6.05;
   };
 
   p.draw = function() {
-    p.background(120,10,100);
-    p.fill(1);
-    if ((p.mouseX > 0) && (p.mouseX < (window.innerWidth/3.05)) && (p.mouseY > 0) && (p.mouseY < (window.innerHeight/3.05))){
-      p.background(20,150,20);
+    // p.fill(1);
+    p.background(255, 151, 148);
+    // console.log(window.innerWidth/3.05)
+    // console.log(window.innerWidth * (2/3.05))
+    p.imageMode(p.CENTER);
+    p.image(img5, centerItemX, centerItemY +15, window.innerWidth / 6, window.innerHeight / 3.5);
+    // textSize = 40;
 
-    x += speed;
-    if(x > p.width){
-      x = 0;
+    p.rectMode(p.CENTER);
+    p.fill(100);
+    // p.rect(centerItemX, centerItemY-10, window.innerWidth/3.05, window.innerHeight/3.05);
+
+    p.rect(centerItemX, centerItemY, 100, 100);
+
+    p.fill(255);
+    p.textAlign(p.CENTER);
+    p.textSize(textSize);
+
+    p.text(num3, centerItemX, centerItemY + 20);
+
+    if ((p.mouseX > 0) && (p.mouseX < tileW) && (p.mouseY > 0) && (p.mouseY < tileH)) {
+
+      // p.background(50,50,100);
+      p.background(255, 151, 148);
+
+      p.imageMode(p.CENTER);
+
+      p.image(img6, centerItemX, centerItemY + 20, 300, 300);
+
+
+      num3 = "";
+      p.textSize(textSize);
+      p.text(num3, centerItemX, centerItemY);
+
+    } else {
+      // p.background(100);
+      // p.background(255, 151, 148);
+      p.imageMode(p.CENTER);
+      p.image(img5, centerItemX, centerItemY + 15, window.innerWidth / 6, window.innerHeight / 3.5);
+
+      num3 = "9";
+      p.textSize(textSize);
+
+      p.text(num3, centerItemX, centerItemY + 20);
+
     }
-    p.ellipse(x,y,50,50);
-  }
-  else{
-    p.background(120,10,100);
 
-  }
+
 
   };
 };
