f599c98614b03371b5446f6cb2fed95a48ad182f
eesomonu1994 <e.esomonu@northeastern.edu>
2020-12-11 16:54:50 -0500

    make responsive add labels

--- household_projects/chart_1/main.js
+++ household_projects/chart_1/main.js
@@ -5,8 +5,10 @@ var margin = {top: 20, right: 20, bottom: 30, left: 40},
 
 
 var svg = d3.select("#chart").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+    // .attr("width", width + margin.left + margin.right)
+    // .attr("height", height + margin.top + margin.bottom)
+            .attr("viewBox", "0 0 " + width + " " + height )
+                          .attr("preserveAspectRatio", "xMidYMid meet")
   .append("g")
     // .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     var color = d3.scaleOrdinal()
@@ -16,12 +18,14 @@ var svg = d3.select("#chart").append("svg")
       // A scale that gives a X target position for each group
 var x = d3.scaleOrdinal()
   // .domain([1, 2, 3])
-  .range([0, 300, 640, 900, 1200])
+  .range([50, width/5,2 * (width/5), 3 *(width/5), 4 * (width/5)])
 
 d3.json("./data/aggregation_query.json", function(error, data) {
   console.log(data);
 
   var node = svg.append("g")
+  // .attr("viewBox", "0 0 " + width+ " " + height )
+  // .attr("preserveAspectRatio", "xMidYMid meet")
   .selectAll("circle")
   .data(data)
   .enter()
@@ -39,18 +43,54 @@ d3.json("./data/aggregation_query.json", function(error, data) {
 
 
     var text = svg.selectAll("text")
+    .attr("class", "labels")
                    .data(data)
                      .enter()
-                     .append("text");
-
+                     .append("text")
     //Add SVG Text Element Attributes
     var textLabels = text
-                .attr("x", function(d) { return (x(d.geo.PUMA)) + 300; })
+                .attr("x", function(d,i) { return x(d.geo.PUMA) + 100; })
                .attr("y", function(d) { return 200; })
-                 .text( function (d) { return d.geo.PUMA; })
-                 .attr("font-family", "sans-serif")
-                .attr("font-size", "20px")
-                 .attr("fill", "black");
+               // .select("p")
+                 .text( function (d,i) {
+                   return d.geo.PUMA;
+             })
+                 // .attr("font-family", "sans-serif")
+                .attr("font-size", "30px");
+
+
+                 // .attr("fill", "black");
+                 var text2 = svg.selectAll("text2")
+                 .attr("class", "labels2")
+                                .data(data)
+                                  .enter()
+                                  .append("text")
+
+                                  var textLabels = text2
+                                              .attr("x", function(d,i) { return x(d.geo.PUMA); })
+                                             .attr("y", function(d) { return 230; })
+                                             // .select("p")
+                                               .text(function(d){
+                                                 if(d.geo.PUMA == "3303"){
+                                                   return "Dorchester & South Boston";
+                                                 }
+                                                 else if(d.geo.PUMA == "3302"){
+                                                   return "Back Bay, Beacon Hill, Charlestown, East Boston, Central & South End";
+
+                                                    }
+                                                    else if(d.geo.PUMA == "3301"){
+                                                      return "Allston, Brighton & Fenway";
+
+                                                       }
+                                                       else if(d.geo.PUMA == "3304"){
+                                                         return "Mattapan & Roxbury";
+
+                                                          }
+                                                          else if(d.geo.PUMA == "3305"){
+                                                              return "Hyde Park, Jamaica Plain, Roslindale & West Roxbury";
+                                                             }
+                                               })
+                                               .attr("font-size", "20px");
 
 // Features of the forces applied to the nodes:
 var simulation = d3.forceSimulation()
