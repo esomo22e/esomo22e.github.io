cf3e9b9227f70fb09daf885f30470ae02e924433
Eunice Esomonu <eesomonu@gmail.com>
2021-03-11 10:02:02 -0500

    redesign maps

--- info-design-2/main.js
+++ info-design-2/main.js
@@ -4,7 +4,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZWVzb21vbnUiLCJhIjoiY2sxaHdjOTE2MGJ3bzNscGtyMXlrM2EzMCJ9.wUCwEKSpzHun34BWXIsgoA';
 var map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    style: 'mapbox://styles/eesomonu/ckm4zoimo0mwz17mpq12lvjbd', // style URL
+    center: [-71.0589, 42.3601], // starting position [lng, lat]
+    zoom: 11 // starting zoom
 });
