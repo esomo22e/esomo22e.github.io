7bca985a486f7cd30bff5253279cecb45130e10d
Eunice Esomonu <eesomonu@gmail.com>
2021-12-05 03:06:56 -0500

    add circles

---
+++ street-maps/main.js
@@ -0,0 +1,74 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZWVzb21vbnUiLCJhIjoiY2sxaHdjOTE2MGJ3bzNscGtyMXlrM2EzMCJ9.wUCwEKSpzHun34BWXIsgoA';
+
+const map = new mapboxgl.Map({
+container: 'map', // container ID
+// style: 'mapbox://styles/eesomonu/ckwln7njj0fjm14phobpmkhg8', // style URL
+style: 'mapbox://styles/mapbox/light-v10',
+center: [-71.0892, 42.3398], // starting position [lng, lat]
+minZoom: 12, // note the camel-case
+    maxZoom: 14
+});
+
+map.on('load', () => {
+
+  map.addSource('earthquakes', {
+type: 'geojson',
+// Use a URL for the value for the `data` property.
+data: {
+'type': 'Feature',
+'geometry': {
+'type': 'Polygon',
+// These coordinates outline Maine.
+'coordinates': [
+[
+[-71.1151, 42.3097],
+[-71.0765, 42.3388],
+[-71.0892, 42.3398]
+]
+]
+}
+}
+});
+
+map.addLayer({
+'id': 'earthquakes-layer',
+'type': 'circle',
+'source': 'earthquakes',
+'paint': {
+'circle-radius': 20,
+'circle-stroke-width': 2,
+'circle-color': 'red',
+'circle-opacity': 0.5,
+'circle-stroke-color': 'white'
+}
+});
+
+// When a click event occurs on a feature in the places layer, open a popup at the
+// location of the feature, with description HTML from its properties.
+map.on('click', 'earthquakes-layer', (e) => {
+// Copy coordinates array.
+const coordinates = e.features[0].geometry.coordinates.slice();
+// const description = e.features[0].properties.description;
+
+ // console.log(e.features[0].coordinates)
+
+ if(coordinates){
+   map.getCanvas().style.cursor = 'pointer'
+
+ }
+// Ensure that if the map is zoomed out such that multiple
+// copies of the feature are visible, the popup appears
+// // over the copy being pointed to.
+// while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+// coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+// }
+//
+// new mapboxgl.Popup()
+// .setLngLat(coordinates)
+// .setHTML(description)
+// .addTo(map);
+});
+//
+
+// });
+});
