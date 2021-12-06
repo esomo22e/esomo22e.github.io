254750b8caf03f355fbad3066a0c68e84f57ef29
Eunice Esomonu <eesomonu@gmail.com>
2021-12-06 07:27:35 -0500

    add dots and slider

--- street-maps/main.js
+++ street-maps/main.js
@@ -4,44 +4,107 @@ const map = new mapboxgl.Map({
 container: 'map', // container ID
 // style: 'mapbox://styles/eesomonu/ckwln7njj0fjm14phobpmkhg8', // style URL
 style: 'mapbox://styles/mapbox/light-v10',
-center: [-71.0892, 42.3398], // starting position [lng, lat]
-minZoom: 12, // note the camel-case
-    maxZoom: 14
+center: [-71.0914, 42.3152],
+zoom: 12.75
+// , // starting position [lng, lat]
+// minZoom: 10, // note the camel-case
+//     maxZoom: 14
 });
 
 map.on('load', () => {
 
-  map.addSource('earthquakes', {
+
+//Get segment of the regular streets
+  map.addSource('street-data', {
+  type: 'geojson',
+  // Use a URL for the value for the `data` property.
+  data: './data/Boston_Street_Segments.geojson'
+  });
+
+  map.addLayer({
+'id': 'streets',
+'type': 'line',
+'source': 'street-data',
+'layout': {
+'line-join': 'round',
+'line-cap': 'round'
+},
+'paint': {
+'line-color': '#888',
+'line-width': 2,
+'line-opacity': 0.4
+}
+});
+
+// Get the segments that I walked around in Back Bay with circles
+map.addSource('walk-data-backBay', {
 type: 'geojson',
 // Use a URL for the value for the `data` property.
-data: {
-'type': 'Feature',
-'geometry': {
-'type': 'Polygon',
-// These coordinates outline Maine.
-'coordinates': [
-[
-[-71.1151, 42.3097],
-[-71.0765, 42.3388],
-[-71.0892, 42.3398]
-]
-]
-}
+data: './data/test.geojson'
+});
+
+
+  map.addLayer({
+'id': 'route',
+'type': 'line',
+'source': 'walk-data-backBay',
+'layout': {
+'line-join': 'round',
+'line-cap': 'round'
+},
+'paint': {
+'line-color': 'magenta',
+'line-width': 4
 }
 });
 
 map.addLayer({
 'id': 'earthquakes-layer',
 'type': 'circle',
-'source': 'earthquakes',
+'source': 'walk-data-backBay',
 'paint': {
-'circle-radius': 20,
+'circle-radius': 7,
 'circle-stroke-width': 2,
-'circle-color': 'red',
+'circle-color': 'purple',
 'circle-opacity': 0.5,
 'circle-stroke-color': 'white'
 }
 });
+
+//
+// // Get the segments that I walked around generally
+// map.addSource('walking-data', {
+// type: 'geojson',
+// // Use a URL for the value for the `data` property.
+// data: './data/walking_maps.geojson'
+// });
+//
+// map.addLayer({
+// 'id': 'route_walking',
+// 'type': 'line',
+// 'source': 'walking-data',
+// 'layout': {
+// 'line-join': 'round',
+// 'line-cap': 'round'
+// },
+// 'paint': {
+// 'line-color': 'magenta',
+// 'line-width': 4
+// }
+// });
+//
+// map.addLayer({
+// 'id': 'circle-walking2',
+// 'type': 'circle',
+// 'source': 'walking-data',
+// 'paint': {
+// 'circle-radius': 10,
+// 'circle-stroke-width': 2,
+// 'circle-color': 'purple',
+// 'circle-opacity': 0.5,
+// 'circle-stroke-color': 'white'
+// }
+// });
 
 // When a click event occurs on a feature in the places layer, open a popup at the
 // location of the feature, with description HTML from its properties.
@@ -50,13 +113,16 @@ map.on('click', 'earthquakes-layer', (e) => {
 const coordinates = e.features[0].geometry.coordinates.slice();
 // const description = e.features[0].properties.description;
 
- // console.log(e.features[0].coordinates)
+ console.log("hello this is clicked")
+ console.log(e.features[0].properties.name)
 
- if(coordinates){
-   map.getCanvas().style.cursor = 'pointer'
+ // if(coordinates){
+ //   map.getCanvas().style.cursor = 'pointer'
+ //
+ // }
+ createDiv(e.features[0].properties.name);
 
- }
-// Ensure that if the map is zoomed out such that multiple
+
 // copies of the feature are visible, the popup appears
 // // over the copy being pointed to.
 // while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
@@ -67,8 +133,44 @@ const coordinates = e.features[0].geometry.coordinates.slice();
 // .setLngLat(coordinates)
 // .setHTML(description)
 // .addTo(map);
+
 });
 //
 
 // });
 });
+var divTitle;
+var t;
+function createDiv(text) {
+
+document.querySelector(".popup-content").style.display = "block";
+
+document.querySelector(".element-title").innerHTML = text;
+document.querySelector(".image-slider").style.display = "block";
+
+document.querySelector(".img_after").src = "./images/img_mountain_before.jpg";
+document.querySelector(".img_before").src = "./images/img_mountain_after.jpg";
+
+
+//  divTitle = document.createElement("div"); // Create the H1 element
+//  divTitle.className = 'active-element'
+// t = document.createTextNode(text); // Create a text element
+// divTitle.appendChild(t); // Append the text node to the H1 element
+//
+// document.querySelector(".popup-content").appendChild(divTitle); // Append the H1 element to the document body
+  // let div = document.createElement('div');
+  // div.innerText = document.getElementById('getText').innerText;
+  // document.body.appendChild(div);
+}// Ensure that if the map is zoomed out such that multiple
+function removeElement() {
+  document.querySelector(".popup-content").style.display = "none";
+  document.querySelector(".element-title").style.display = "none";
+  document.querySelector(".image-slider").style.display = "none";
+
+  // var h = document.removeElement("H2"); // Create the H1 element
+  // var t = document.removeTextNode("Your H2 text"); // Create a text element
+  // h .removeChild(t); // Append the text node to the H1 element
+
+  document.querySelector(".popup-content").removeChild(h); // Append the H1 element to the document body
+
+}
