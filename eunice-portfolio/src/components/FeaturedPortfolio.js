3d710110ad32d77dc9c7bfe6c3cfed14a3af06c6
Esomonu, Eunice <eesomonu@ap.org>
2024-01-28 22:03:01 -0500

    add featured height column

--- eunice-portfolio/src/components/FeaturedPortfolio.js
+++ eunice-portfolio/src/components/FeaturedPortfolio.js
@@ -1,46 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 import GridItem from "./GridItem";
+import portfolioItems from "../data/portfolioData.json"; // Import your portfolio data
 
 const FeaturedWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 20px;
+  padding: 20px;
 `;
-
-const portfolioItems = [
-  {
-    title: "Item 1",
-    description: "Description 1",
-    imgUrl: "url1.jpg",
-  },
-  {
-    title: "Item 2",
-    description: "Description 2",
-    imgUrl: "url2.jpg",
-  },
-  {
-    title: "Item 1",
-    description: "Description 1",
-    imgUrl: "url3.jpg",
-  },
-  {
-    title: "Item 2",
-    description: "Description 2",
-    imgUrl: "url4.jpg",
-  },
-
-  {
-    title: "Item 1",
-    description: "Description 1",
-    imgUrl: "url5.jpg",
-  },
-  {
-    title: "Item 2",
-    description: "Description 2",
-    imgUrl: "url6.jpg",
-  },
-];
 
 function FeaturedPortfolio() {
   return (
--- eunice-portfolio/src/components/GridItem.js
+++ eunice-portfolio/src/components/GridItem.js
@@ -1,11 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 
-const GridContainer = styled.div``;
+const GridContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 250px;
+  border: 1px solid #ccc;
+`;
 
-const GridImage = styled.image``;
+const GridImage = styled.image`
+  width: 100%;
+  height: auto;
+  max-height: 100%;
+`;
 
-const DetailsContainer = styled.div``;
+const DetailsContainer = styled.div`
+  padding: 10px;
+`;
 const GridTitle = styled.h3``;
 
 const GridDesc = styled.div``;
