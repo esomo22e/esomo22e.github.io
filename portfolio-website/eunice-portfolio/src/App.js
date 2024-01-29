8c00f5ee05035bd29fe8dac5869ec5a09ab50fb7
Esomonu, Eunice <eesomonu@ap.org>
2024-01-28 19:07:35 -0500

    create new site

--- portfolio-website/eunice-portfolio/src/App.js
+++ portfolio-website/eunice-portfolio/src/App.js
@@ -1,27 +1,17 @@
-import logo from './logo.svg';
+import logo from "./logo.svg";
 import Container from "react-bootstrap/Container";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import React from "react";
+import Header from "./components/Header.js";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello World! This show is weird!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header"></header> */}
+      <Header></Header>
+      <logo></logo>
     </div>
   );
 }
